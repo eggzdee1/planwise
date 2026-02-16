b11cd8fb0e0de323b3813679e1966b9671d1ebaf
Victor Li <_victorli_@berkeley.edu>
2026-02-15 23:00:19 -0800

    Prepare backend for Render deploy.

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ app.get("/", (_req, res) => {
   res.send("API running");
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
